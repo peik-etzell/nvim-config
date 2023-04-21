9b234ade28a05a4c96c257e10182b782e35b5395
Peik Etzell <peik.etzell@pemamek.com>
2023-04-21 18:36:41 +0300

    Switch back to toggleterm

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,24 +1,6 @@
 return {
-	-- Themes
-	-- Statusline
-
 	"Pocco81/auto-save.nvim",
 	"fabi1cazenave/suckless.vim",
-	{
-		"fabi1cazenave/termopen.vim",
-		keys = {
-			{ "<C-CR>", ":call TermOpen()<CR>" },
-		},
-	},
-
-	-- {
-	-- 	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-	-- 	config = function()
-	-- 		require("lsp_lines").setup({
-	-- 			virtual_lines = { only_current_line = true },
-	-- 		})
-	-- 	end,
-	-- },
 	{
 		"alexghergh/nvim-tmux-navigation",
 		lazy = true,
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = true,
+		opts = {},
+		keys = {
+			{ "<A-CR>", ":ToggleTerm<CR>" },
+		},
+	},
+}
