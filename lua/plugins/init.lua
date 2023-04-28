6407bcc49fb5972aaef8844d311243980106e1d6
Peik Etzell <peik.etzell@gmail.com>
2023-04-28 17:45:20 +0300

    Cleaning up

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,21 +1,3 @@
 return {
 	"Pocco81/auto-save.nvim",
-	-- "fabi1cazenave/suckless.vim",
-	{
-		"alexghergh/nvim-tmux-navigation",
-		lazy = true,
-		config = function()
-			require("nvim-tmux-navigation").setup({
-				disable_when_zoomed = true, -- defaults to false
-				keybindings = {
-					left = "<C-h>",
-					down = "<C-j>",
-					up = "<C-k>",
-					right = "<C-l>",
-					last_active = "<C-\\>",
-					next = "<C-Space>",
-				},
-			})
-		end,
-	},
 }
