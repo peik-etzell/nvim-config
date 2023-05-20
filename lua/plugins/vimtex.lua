42c73f8627c37082e1c5d44225c9d6869c96aafc
Peik Etzell <peik.etzell@gmail.com>
2023-05-20 19:21:57 +0300

    Fix vimtex keymaps

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,11 +2,14 @@ return {
 	{
 		"lervag/vimtex",
 		ft = "tex",
-		keys = {
-			{ "j", "gj" },
-			{ "k", "gk" },
-			{ "$", "g$" },
-			{ "0", "g0" },
-		},
+		config = function()
+			local set_keymap_n = function(lhs, rhs)
+				vim.api.nvim_set_keymap("n", lhs, rhs, {})
+			end
+			set_keymap_n("j", "gj")
+			set_keymap_n("k", "gk")
+			set_keymap_n("$", "g$")
+			set_keymap_n("0", "g0")
+		end,
 	},
 }
