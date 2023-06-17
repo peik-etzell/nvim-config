4f060510b09c52826c6aa27973a736bdfbe0f552
Peik Etzell <peik.etzell@gmail.com>
2023-06-17 13:19:43 +0300

    Add mason-tool-installer

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,19 +20,7 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				automatic_installation = true,
-				ensure_installed = {
-					"clangd",
-					"html",
-					"yamlls",
-					"pylsp",
-					"pyright",
-					"jsonls",
-					"lua_ls",
-					"marksman",
-					"bashls",
-					"docker_compose_language_service",
-					"dockerls",
-				},
+				ensure_installed = {}, -- mason-tool-installer instead
 			})
 			require("mason-lspconfig").setup_handlers({
 				function(server_name)
@@ -45,43 +33,4 @@ return {
 			})
 		end,
 	},
-	-- {
-	-- 	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	-- 	dependencies = {
-	-- 		"williamboman/mason.nvim",
-	-- 	},
-	-- 	config = function()
-	-- 		require("mason-tool-installer").setup({
-	-- 			ensure_installed = {
-	-- 				-- Lua
-	-- 				"lua-language-server",
-	-- 				"stylua",
-	-- 				"luacheck",
-	--
-	-- 				-- sh
-	-- 				"shellcheck",
-	-- 				"shfmt",
-	-- 				"beautysh",
-	--
-	-- 				"editorconfig-checker",
-	-- 				"staticcheck",
-	-- 				"autopep8",
-	-- 				"lemminx", -- xmlformatter
-	--
-	-- 				-- C++
-	-- 				"clang-format",
-	-- 				"cpplint",
-	-- 				"cpptools",
-	-- 				"codelldb",
-	-- 				"cmake-language-server",
-	-- 			},
-	--
-	-- 			run_on_start = true,
-	--
-	-- 			start_delay = 3000, -- 3 second delay
-	--
-	-- 			debounce_hours = 5, -- at least 5 hours between attempts to install/update
-	-- 		})
-	-- 	end,
-	-- },
 }
