964dfacaef011829d1865be1110c1381a3842bc4
Peik Etzell <peik.etzell@gmail.com>
2023-08-01 08:53:03 +0300

    Add .typ as typst file alias

--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
 vim.g.do_filetype_lua = 1
 vim.filetype.add({
-    extension = {
-        repos = "yaml",
-        rviz = "yaml",
-        scad = "openscad",
-    },
+	extension = {
+		repos = "yaml",
+		rviz = "yaml",
+		scad = "openscad",
+		typ = "typst",
+	},
 })
 
 vim.g.mapleader = " "
@@ -51,19 +52,19 @@ require("window-management")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
-    ui = { border = vim.g.border },
-    change_detection = { notify = false },
+	ui = { border = vim.g.border },
+	change_detection = { notify = false },
 })
 
 require("theme")
