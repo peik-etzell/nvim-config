491bd0a7b0e8f7287fd2a49a118572a3790b122a
Peik Etzell <peik.etzell@gmail.com>
2023-08-09 17:26:04 +0300

    Use c++17 for null-ls.clang_check

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -8,24 +8,20 @@ return {
 			null_ls.setup({
 				sources = {
 					builtins.formatting.stylua,
-					-- builtins.formatting.clang_format.with({
-					-- 	-- extra_args = { "--style", "{IndentWidth: 4}" },
-					-- }),
 					builtins.formatting.prettierd,
 					builtins.formatting.xmlformat,
 					builtins.formatting.clang_format,
 					builtins.code_actions.eslint_d,
 					builtins.code_actions.refactoring,
-					builtins.diagnostics.clang_check,
+					builtins.diagnostics.clang_check.with({
+						extra_args = { "-extra-arg=-std=c++17" },
+					}),
 					builtins.diagnostics.cpplint.with({
 						args = { "--linelength=120" }, -- ROS
 					}),
 					builtins.diagnostics.cppcheck.with({
 						extra_args = { "--language=c++", "--std=c++17" },
 					}),
-					-- builtins.diagnostics.eslint,
-					-- builtins.completion.spell,
-					-- builtins.code_actions.proselint,
 				},
 			})
 		end,
