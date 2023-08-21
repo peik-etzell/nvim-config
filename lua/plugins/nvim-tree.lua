cd6e9c592b283e4f682f77b7cbc0ace81a9eb91d
Peik Etzell <peik.etzell@gmail.com>
2023-08-21 08:26:00 +0300

    Disable nvim_tree open-on-startup

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -35,7 +35,7 @@ return {
                 require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
             end
 
-            vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+            -- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
         end,
         dependencies = { "nvim-tree/nvim-web-devicons" },
         keys = {
