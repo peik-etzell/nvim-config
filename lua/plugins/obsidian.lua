27eaed60886f4af1ddfd240c82464c4db5e4e2dd
Peik Etzell <peik.etzell@gmail.com>
2023-09-04 14:59:42 +0300

    Add obsidian plugin

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,23 @@
+return {
+	{
+
+		"epwalsh/obsidian.nvim",
+		lazy = true,
+		event = { "BufReadPre " .. vim.fn.expand("~") .. "/Obsidian/**.md" },
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
+
+			-- see below for full list of optional dependencies 👇
+		},
+		opts = {
+			dir = "~/Obsidian", -- no need to call 'vim.fn.expand' here
+
+			-- see below for full list of options 👇
+			completion = {
+				nvim_cmp = true,
+			},
+			mappings = {},
+		},
+	},
+}
