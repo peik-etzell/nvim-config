6ed9afcff0a7849ff8818737a5b0abab5146fa2f
Peik Etzell <peik.etzell@gmail.com>
2023-09-10 15:34:20 +0300

    More Null-ls sources enabled

--- lua/plugins/mason-tools.lua
+++ lua/plugins/mason-tools.lua
@@ -40,6 +40,7 @@ return {
                     "autopep8",
                     "python-lsp-server",
                     "debugpy",
+                    "pylint",
 
                     -- Webdev
                     "css-lsp",
@@ -65,13 +66,14 @@ return {
                     -- "editorconfig-checker",
                     -- "staticcheck",
                     "marksman",
+                    "markdownlint",
                 },
 
                 -- if set to true this will check each tool for updates. If updates
                 -- are available the tool will be updated. This setting does not
                 -- affect :MasonToolsUpdate or :MasonToolsInstall.
                 -- Default: false
-                auto_update = false,
+                auto_update = true,
 
                 -- automatically install / update on startup. If set to false nothing
                 -- will happen on startup. You can use :MasonToolsInstall or
@@ -83,7 +85,7 @@ return {
                 -- effective if run_on_start is set to true.
                 -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
                 -- Default: 0
-                start_delay = 3000, -- 3 second delay
+                start_delay = 1000, -- 3 second delay
 
                 -- Only attempt to install if 'debounce_hours' number of hours has
                 -- elapsed since the last time Neovim was started. This stores a
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,30 +1,48 @@
 return {
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local null_ls = require("null-ls")
-			local builtins = null_ls.builtins
-			null_ls.setup({
-				sources = {
-					builtins.formatting.stylua,
-					builtins.formatting.prettierd,
-					builtins.formatting.xmlformat,
-					builtins.formatting.clang_format,
-					builtins.formatting.latexindent,
-					builtins.code_actions.eslint_d,
-					builtins.code_actions.refactoring,
-					-- builtins.diagnostics.clang_check.with({
-					-- 	extra_args = { "-extra-arg=-std=c++17" },
-					-- }),
-					builtins.diagnostics.cpplint.with({
-						args = { "--linelength=120" }, -- ROS
-					}),
-					builtins.diagnostics.cppcheck.with({
-						extra_args = { "--language=c++", "--std=c++17" },
-					}),
-				},
-			})
-		end,
-	},
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local null_ls = require("null-ls")
+            local builtins = null_ls.builtins
+            null_ls.setup({
+                sources = {
+                    -- Lua
+                    -- builtins.formatting.stylua,
+                    builtins.diagnostics.luacheck,
+
+                    builtins.formatting.prettierd,
+                    builtins.formatting.xmlformat,
+                    builtins.formatting.latexindent,
+
+                    -- Shell
+                    builtins.formatting.shfmt,
+                    builtins.diagnostics.shellcheck,
+
+                    -- Python
+                    builtins.diagnostics.pylint,
+                    builtins.formatting.autopep8,
+
+                    builtins.code_actions.eslint_d,
+                    builtins.code_actions.refactoring,
+
+                    -- Markdown
+                    -- builtins.formatting.markdownlint,
+                    builtins.diagnostics.markdownlint,
+
+                    -- C++
+                    -- builtins.diagnostics.clang_check.with({
+                    -- 	extra_args = { "-extra-arg=-std=c++17" },
+                    -- }),
+                    builtins.diagnostics.cpplint.with({
+                        args = { "--linelength=120" }, -- ROS
+                    }),
+                    builtins.diagnostics.cppcheck.with({
+                        extra_args = { "--language=c++", "--std=c++17" },
+                    }),
+                    builtins.formatting.clang_format,
+                },
+            })
+        end,
+    },
 }
