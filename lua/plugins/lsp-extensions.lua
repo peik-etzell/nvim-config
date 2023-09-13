7896d5bf38059e76d12846a3b085ab73d7e8197b
Peik Etzell <peik.etzell@gmail.com>
2023-09-13 20:37:33 +0300

    Add inlay_hint(), move clangd_extensions to lsp.lua

--- lua/plugins/lsp-extensions.lua
+++ lua/plugins/lsp-extensions.lua
@@ -1,47 +1,24 @@
 return {
-	{
-		"p00f/clangd_extensions.nvim",
-		ft = { "c", "cpp" },
-		lazy = true,
-		commit = "707f8633b84f9b72bcf811c34d383a297a25e8a0",
-		config = function()
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities.offsetEncoding = "utf-16"
-			require("clangd_extensions").setup({
-				server = {
-					-- options to pass to nvim-lspconfig
-					-- i.e. the arguments to require("lspconfig").clangd.setup({})
-					capabilities = capabilities,
-				},
-				extensions = {
-					autoSetHints = false,
-					inlay_hints = {
-						inline = false,
-					},
-				},
-			})
-		end,
-	},
-	{
-		"scalameta/nvim-metals",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		ft = "scala",
-		lazy = true,
-	},
-	{
-		"simrat39/rust-tools.nvim",
-		ft = "rust",
-		lazy = true,
-		dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" },
-	},
-	{
-		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-		config = function()
-			require("lsp_lines").setup()
-			vim.diagnostic.config({
-				virtual_text = false,
-			})
-		end,
-		ft = { "rust" },
-	},
+    {
+        "scalameta/nvim-metals",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        ft = "scala",
+        lazy = true,
+    },
+    {
+        "simrat39/rust-tools.nvim",
+        ft = "rust",
+        lazy = true,
+        dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" },
+    },
+    {
+        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+        config = function()
+            require("lsp_lines").setup()
+            vim.diagnostic.config({
+                virtual_text = false,
+            })
+        end,
+        ft = { "rust" },
+    },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,60 +1,62 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-				border = vim.g.border,
-			})
-			require("lspconfig.ui.windows").default_options.border = vim.g.border
-			vim.api.nvim_create_autocmd("LspAttach", {
-				desc = "LSP on_attach",
-				callback = function(event)
-					local set_keymap = function(lhs, rhs)
-						vim.keymap.set("n", lhs, rhs, { silent = true, buffer = event.buf })
-					end
-					set_keymap("K", vim.lsp.buf.hover)
-					set_keymap("<leader>rn", vim.lsp.buf.rename)
-					set_keymap("<C-k>", function()
-						vim.diagnostic.open_float({ border = vim.g.border })
-					end)
-					set_keymap("<leader>s", function()
-						vim.lsp.buf.format({ async = true })
-					end)
-				end,
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		lazy = true,
-		config = function()
-			require("mason").setup({
-				install_root_dir = vim.fn.stdpath("data") .. "/mason",
-				ui = {
-					border = vim.g.border,
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					local setup = require("lspconfig")[server_name].setup
-					local capabilities = require("cmp_nvim_lsp").default_capabilities()
-					if server_name ~= "clangd" then
-						setup({
-							capabilities = capabilities,
-						})
-					end
-				end,
-			})
-		end,
-	},
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+                border = vim.g.border,
+            })
+            require("lspconfig.ui.windows").default_options.border = vim.g.border
+            vim.api.nvim_create_autocmd("LspAttach", {
+                desc = "LSP on_attach",
+                callback = function(event)
+                    local client = vim.lsp.get_client_by_id(event.data.client_id)
+                    if client.server_capabilities.inlayHintProvider then
+                        vim.lsp.inlay_hint(event.buf, true)
+                    end
+
+                    local set_keymap = function(lhs, rhs)
+                        vim.keymap.set("n", lhs, rhs, { silent = true, buffer = event.buf })
+                    end
+                    set_keymap("K", vim.lsp.buf.hover)
+                    set_keymap("<leader>rn", vim.lsp.buf.rename)
+                    set_keymap("<C-k>", function()
+                        vim.diagnostic.open_float({ border = vim.g.border })
+                    end)
+                    set_keymap("<leader>s", function()
+                        vim.lsp.buf.format({ async = true })
+                    end)
+                end,
+            })
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        lazy = true,
+        config = function()
+            require("mason").setup({
+                install_root_dir = vim.fn.stdpath("data") .. "/mason",
+                ui = {
+                    border = vim.g.border,
+                },
+            })
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "neovim/nvim-lspconfig",
+            "p00f/clangd_extensions.nvim",
+        },
+        config = function()
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = require("cmp_nvim_lsp").default_capabilities()
+                    }
+                end,
+            })
+        end,
+    },
 }
