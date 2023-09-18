075b55aa7553b5eddda20f94abde9887f991abc6
Peik Etzell <peik.etzell@gmail.com>
2023-09-18 08:48:55 +0300

    Clean up theme.lua

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,16 +1,18 @@
-local config_path = vim.fn.stdpath("config")
+local config_path = vim.fn.stdpath("state") .. "/theme"
 
-
-local function write_theme()
+local function write_current_theme()
     local theme = vim.g.colors_name
-    local f = assert(io.open(config_path .. "/theme", "w"))
+    local f = io.open(config_path, "w")
+    if not f then return end
     f:write(theme)
     f:close()
 end
 
-local function read_theme()
-    local f = assert(io.open(config_path .. "/theme", "r"))
+local function get_stored_theme()
+    local f = io.open(config_path, "r")
+    if not f then return "" end
     local stored_theme = f:read("*all")
+    f:close()
     return stored_theme
 end
 
@@ -19,11 +21,11 @@ local function change_theme(theme)
 end
 
 vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-    callback = write_theme,
+    callback = write_current_theme,
 })
 
-local persisted_theme = read_theme()
-
+-- Runs on startup:
+local persisted_theme = get_stored_theme()
 if persisted_theme ~= "" then
     change_theme(persisted_theme)
 else
