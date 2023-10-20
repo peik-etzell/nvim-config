62617e7ca8a0326143c78ded45e94e0c61a4161e
Peik Etzell <peik.etzell@gmail.com>
2023-10-20 11:49:41 +0300

    Move null-ls to none-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,6 +1,6 @@
 return {
     {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local null_ls = require("null-ls")
