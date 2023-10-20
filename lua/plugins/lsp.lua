4ee5977d0d0825a85a114d77f66e8ddce8b08e15
Peik Etzell <peik.etzell@gmail.com>
2023-10-20 11:49:27 +0300

    Force clangd offset-encoding

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -70,10 +70,18 @@ return {
         config = function()
             require("mason-lspconfig").setup_handlers({
                 function(server_name)
-                    require("lspconfig")[server_name].setup {
+                    require("lspconfig")[server_name].setup({
                         capabilities = require("cmp_nvim_lsp").default_capabilities()
-                    }
+                    })
                 end,
+                ['clangd'] = function()
+                    require("lspconfig")['clangd'].setup({
+                        capabilities = vim.tbl_extend(
+                            'force',
+                            require("cmp_nvim_lsp").default_capabilities(),
+                            { offsetEncoding = 'utf-8' })
+                    })
+                end
             })
         end,
     },
