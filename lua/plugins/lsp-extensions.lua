9e56818ee74be5a5ec2eed09220688e219464524
Peik Etzell <peik.etzell@gmail.com>
2023-10-25 18:08:10 +0300

    Add nvim-metals config

--- lua/plugins/lsp-extensions.lua
+++ lua/plugins/lsp-extensions.lua
@@ -1,9 +1,29 @@
 return {
     {
         "scalameta/nvim-metals",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
         ft = "scala",
         lazy = true,
+        config = function()
+            local metals = require('metals')
+            local config = metals.bare_config()
+            config.settings = {}
+            config.capabilities = require('cmp_nvim_lsp').default_capabilities()
+            config.on_attach = function()
+                metals.setup_dap()
+            end
+            local group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = { 'scala', 'sbt', 'java' },
+                callback = function()
+                    metals.initialize_or_attach(config)
+                end,
+                group = group
+            })
+        end,
     },
     {
         "simrat39/rust-tools.nvim",
