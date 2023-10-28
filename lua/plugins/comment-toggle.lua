cd54b82ce23915cdc7e9f566143b70e6cec53ccb
Peik Etzell <peik.etzell@gmail.com>
2023-10-28 11:57:47 +0300

    Make comment-toggle silent

--- lua/plugins/comment-toggle.lua
+++ lua/plugins/comment-toggle.lua
@@ -1,12 +1,12 @@
 return {
     {
         "terrortylor/nvim-comment",
+        lazy = false,
         config = function()
             require("nvim_comment").setup()
+            vim.keymap.set({ 'n', 'v' }, '<leader>/', function ()
+                vim.cmd([[CommentToggle]])
+            end, { silent = true })
         end,
-        keys = {
-            { "<leader>/", ":CommentToggle<CR>", mode = "n" },
-            { "<leader>/", ":CommentToggle<CR>", mode = "v" },
-        },
     },
 }
