d47295ab3e8b6888746ee314be30598ec9ff9729
Peik Etzell <peik.etzell@gmail.com>
2023-10-28 11:43:44 +0300

    Move autopairs to own file

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            local autopairs = require("nvim-autopairs")
+            autopairs.setup()
+            local rule = require("nvim-autopairs.rule")
+            local tex = { 'tex', 'latex' }
+            autopairs.add_rules({
+                rule('$', '$', tex),
+                rule('\\[', '\\]', tex),
+                rule('\\(', '\\)', tex)
+            })
+        end,
+    },
+}
--- lua/plugins/comment-toggle.lua
+++ lua/plugins/comment-toggle.lua
@@ -9,19 +9,4 @@ return {
             { "<leader>/", ":CommentToggle<CR>", mode = "v" },
         },
     },
-    {
-        "windwp/nvim-autopairs",
-        config = function()
-            local autopairs = require("nvim-autopairs")
-            autopairs.setup()
-            local rule = require("nvim-autopairs.rule")
-            local tex = { 'tex', 'latex' }
-            autopairs.add_rules({
-                rule('$$', '$$', tex),
-                rule('$', '$', tex),
-                rule('\\[', '\\]', tex),
-                rule('\\(', '\\)', tex)
-            })
-        end,
-    },
 }
