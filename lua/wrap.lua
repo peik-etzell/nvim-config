d120d05e6235d4715d80288f54f13ea1ff797e63
Peik Etzell <peik.etzell@gmail.com>
2023-11-07 10:56:13 +0200

    Set wrap to buffer, making turnign it on and off unnecessary

--- lua/wrap.lua
+++ lua/wrap.lua
@@ -5,26 +5,16 @@ local pattern = { '*.md', '*.tex', '*.txt' }
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     pattern = pattern,
     callback = function(_)
-        vim.opt.wrap = true
-        vim.opt.linebreak = true
 
         local set_keymap_n = function(lhs, rhs)
             vim.keymap.set('n', lhs, rhs, { silent = true, buffer = true }) -- Buffer-local binds
             vim.keymap.set('v', lhs, rhs, { silent = true, buffer = true }) -- Buffer-local binds
+        vim.opt_local.wrap = true
+        vim.opt_local.linebreak = true
         end
         set_keymap_n('j', 'gj')
         set_keymap_n('k', 'gk')
         set_keymap_n('$', 'g$')
         set_keymap_n('0', 'g0')
-    end,
-})
-
--- Turn off wrap when leaving a prose-based file
-vim.api.nvim_create_autocmd({ 'BufLeave' }, {
-    pattern = pattern,
-    callback = function(_)
-        local set = vim.opt
-        set.wrap = false
-        set.linebreak = false
     end,
 })
