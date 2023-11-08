c0cf3b5dae8b2dca4ebe87a7ef60eb6530e5aebd
Peik Etzell <peik.etzell@gmail.com>
2023-11-08 17:30:46 +0200

    Add nightfox.nvim, simplify themes.lua

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,39 +4,13 @@ return {
         branch = '0.0.x',
         priority = 1000,
     },
+    { 'EdenEast/nightfox.nvim', priority = 1000 },
     { 'ellisonleao/gruvbox.nvim', priority = 1000 },
     { 'catppuccin/nvim', priority = 1000 },
-    {
-        'nvim-tree/nvim-web-devicons',
-        config = function()
-            require('nvim-web-devicons').setup({
-                color_icons = true,
-                -- override_by_filename = { ["gitignore"] = { icon = " ", color = "#f1502f", name = "Gitignore" } },
-            })
-        end,
-    },
-    {
-        'lukas-reineke/indent-blankline.nvim',
-        enabled = false,
-        config = function()
-            require('indent_blankline').setup({
-                show_current_context = true,
-                show_current_context_start = true,
-            })
-        end,
-    },
-    {
-        'lewis6991/gitsigns.nvim',
-        init = function()
-            require('gitsigns').setup()
-        end,
-    },
+    { 'nvim-tree/nvim-web-devicons', opts = { color_icons = true } },
+    { 'lewis6991/gitsigns.nvim', opts = {} },
     {
         'peik-etzell/persist-theme.nvim',
-        config = function()
-            require('persist-theme').setup({
-                default_colorscheme = 'catppuccin',
-            })
-        end,
+        opts = { default_colorscheme = 'catppuccin' },
     },
 }
