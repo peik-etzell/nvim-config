148cf514ed9035a8a6e67aefe9f33a86b92844e8
Peik Etzell <peik.etzell@gmail.com>
2023-11-17 18:20:59 +0200

    Md plugin to git recommended

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,14 @@
 return {
     {
         'iamcco/markdown-preview.nvim',
-        ft = 'markdown',
-        config = function()
-            -- vim.fn["mkdp#util#install"]()
+        cmd = {
+            'MarkdownPreviewToggle',
+            'MarkdownPreview',
+            'MarkdownPreviewStop',
+        },
+        ft = { 'markdown' },
+        build = function()
+            vim.fn['mkdp#util#install']()
         end,
     },
 }
