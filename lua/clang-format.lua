4b73edbc79bae47be68849f0b529d755f36949ec
Peik Etzell <peik.etzell@gmail.com>
2023-11-21 18:45:35 +0200

    Improve clang format integration

--- lua/clang-format.lua
+++ lua/clang-format.lua
@@ -1,10 +1,17 @@
-local handle = io.popen(
-    "clang-format -dump-config | grep ^IndentWidth: | awk '{print $2}'"
-)
-if handle then
-    local indent_width = handle:read('*a')
-    handle:close()
+local pattern = { '*.c', '*.cpp' }
 
-    vim.opt.tabstop = tonumber(indent_width)
-    vim.opt.shiftwidth = tonumber(indent_width)
-end
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    pattern = pattern,
+    callback = function(_)
+        local handle = io.popen(
+            "clang-format -dump-config | grep ^IndentWidth: | awk '{print $2}'"
+        )
+        if handle then
+            local indent_width = tonumber(handle:read('*a'))
+            handle:close()
+
+            vim.opt_local.tabstop = indent_width
+            vim.opt_local.shiftwidth = indent_width
+        end
+    end,
+})
