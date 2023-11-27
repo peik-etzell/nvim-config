8261a31d9375e118e6cc23162a6fd5c82437b37a
Peik Etzell <peik.etzell@gmail.com>
2023-11-27 18:10:29 +0200

    Change conceal color

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,7 +4,19 @@ return {
         branch = '0.0.x',
         priority = 1000,
     },
-    { 'EdenEast/nightfox.nvim', priority = 1000 },
+    {
+        'EdenEast/nightfox.nvim',
+        priority = 1000,
+        opts = {
+            groups = {
+                all = {
+                    -- Some examples, make sure only to define Conceal once
+                    -- Conceal = { link = 'Comment' }, -- link `Conceal` to another highlight group
+                    Conceal = { fg = 'syntax.number' }, -- link `Conceal` to a spec value
+                },
+            },
+        },
+    },
     { 'ellisonleao/gruvbox.nvim', priority = 1000 },
     { 'catppuccin/nvim', priority = 1000 },
     { 'nvim-tree/nvim-web-devicons', opts = { color_icons = true } },
