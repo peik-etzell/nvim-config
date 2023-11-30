616728c8a6f5e4cfdb226f6851de1209c2231765
Peik Etzell <peik.etzell@gmail.com>
2023-11-30 12:43:41 +0200

    Add qmk.nvim

---
+++ lua/plugins/qmk.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        'codethread/qmk.nvim',
+        opts = {
+            name = 'LAYOUT_ortho_4x12',
+            layout = {
+                'x x x x x x x x x x x x',
+                'x x x x x x x x x x x x',
+                'x x x x x x x x x x x x',
+                'x x x x x x x x x x x x',
+            },
+        },
+    },
+}
