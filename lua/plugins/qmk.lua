184fed52dd4fa3d2e777267dba4fd50becd11425
Peik Etzell <peik.etzell@gmail.com>
2023-12-10 17:11:49 +0200

    Qmk and kitty plugin load on event

--- lua/plugins/qmk.lua
+++ lua/plugins/qmk.lua
@@ -1,6 +1,7 @@
 return {
     {
         'codethread/qmk.nvim',
+        event = 'BufEnter keymap.c',
         opts = {
             name = 'LAYOUT_ortho_4x12',
             layout = {
--- lua/plugins/vim-kitty.lua
+++ lua/plugins/vim-kitty.lua
@@ -1,5 +1,9 @@
 return {
     {
         'fladson/vim-kitty',
+        event = 'BufEnter kitty.conf',
+        config = function()
+            vim.o.filetype = 'kitty'
+        end,
     },
 }
