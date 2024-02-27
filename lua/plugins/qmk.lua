9e2039505088cd586095e3aa3fa6dc6045e9b240
Peik Etzell <peik.etzell@gmail.com>
2024-02-27 21:03:20 +0200

    Add cantor to qmk config

--- lua/plugins/qmk.lua
+++ lua/plugins/qmk.lua
@@ -2,14 +2,46 @@ return {
     {
         'codethread/qmk.nvim',
         event = 'BufEnter keymap.c',
-        opts = {
-            name = 'LAYOUT_ortho_4x12',
-            layout = {
-                'x x x x x x x x x x x x',
-                'x x x x x x x x x x x x',
-                'x x x x x x x x x x x x',
-                'x x x x x x x x x x x x',
-            },
-        },
+        config = function()
+            local qmk = require('qmk')
+
+            local group = vim.api.nvim_create_augroup('My qmk', {})
+
+            vim.api.nvim_create_autocmd('BufEnter', {
+                desc = 'Format cantor keymap',
+                group = group,
+                pattern = '*cantor/keymaps/*/keymap.c',
+                callback = function()
+                    print('Qmk init for cantor')
+                    qmk.setup({
+                        name = 'LAYOUT_split_3x6_3',
+                        layout = {
+                            'x x x x x x _ _ _ x x x x x x',
+                            'x x x x x x _ _ _ x x x x x x',
+                            'x x x x x x _ _ _ x x x x x x',
+                            '_ _ _ _ x x x _ x x x _ _ _ _',
+                        },
+                    })
+                end,
+            })
+
+            vim.api.nvim_create_autocmd('BufEnter', {
+                desc = 'Format jj40 keymap',
+                group = group,
+                pattern = '*jj40/keymaps/*/keymap.c',
+                callback = function()
+                    print('Qmk init for jj40')
+                    qmk.setup({
+                        name = 'LAYOUT_ortho_4x12',
+                        layout = {
+                            'x x x x x x x x x x x x',
+                            'x x x x x x x x x x x x',
+                            'x x x x x x x x x x x x',
+                            'x x x x x x x x x x x x',
+                        },
+                    })
+                end,
+            })
+        end,
     },
 }
