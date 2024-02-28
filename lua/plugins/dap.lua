8df40a88a1098351fe415213611802116de2e03e
Peik Etzell <peik.etzell@gmail.com>
2024-02-28 16:09:38 +0200

    Refactor dap.lua, lazier + gdb

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,7 +2,6 @@ return {
     {
         'rcarriga/nvim-dap-ui',
         ft = { 'c', 'cpp', 'rust', 'python', 'csharp' },
-        event = 'VeryLazy',
         dependencies = {
             {
                 'mfussenegger/nvim-dap',
@@ -10,14 +9,24 @@ return {
                     local dap = require('dap')
                     local adapters = dap.adapters
                     local configurations = dap.configurations
+                    local nixos = vim.fn.filereadable('/etc/NIXOS') ~= 0
 
                     -- ADAPTERS
+                    adapters.gdb = {
+                        type = 'executable',
+                        command = 'gdb',
+                        args = { '-i', 'dap' },
+                    }
+
                     adapters.lldb = {
                         type = 'executable',
-                        command = vim.fn.stdpath('data')
-                            .. '/mason/packages/codelldb/codelldb',
+                        command = nixos
+                                and '/run/current-system/sw/bin/lldb-vscode'
+                            or vim.fn.stdpath('data')
+                                .. '/mason/packages/codelldb/codelldb',
                         name = 'lldb',
                     }
+
                     adapters.cppdbg = {
                         id = 'cppdbg',
                         name = 'cppdbg',
@@ -43,7 +52,7 @@ return {
 
                     -- CONFIGURATIONS
                     local lldbConfig = {
-                        name = 'Launch lldb',
+                        name = 'LLDB',
                         type = 'lldb',
                         request = 'launch',
                         program = path_to_executable,
@@ -52,7 +61,7 @@ return {
                         args = {},
                     }
                     local cpptoolsConfig = {
-                        name = 'Launch cpptools',
+                        name = 'cpptools',
                         type = 'cppdbg',
                         request = 'launch',
                         program = path_to_executable,
@@ -67,7 +76,7 @@ return {
                         },
                     }
                     local gdbserverConfig = {
-                        name = 'Attach to gdbserver :1234',
+                        name = 'gdbserver at localhost:1234',
                         type = 'cppdbg',
                         request = 'launch',
                         MIMode = 'gdb',
@@ -83,9 +92,23 @@ return {
                             },
                         },
                     }
-                    configurations.cpp = { cpptoolsConfig, gdbserverConfig }
-                    configurations.c = { cpptoolsConfig, gdbserverConfig }
-                    configurations.rust = { cpptoolsConfig, gdbserverConfig }
+                    local gdbConfig = {
+                        name = 'GDB',
+                        type = 'gdb',
+                        request = 'launch',
+                        program = path_to_executable,
+                        cwd = '${workspaceFolder}',
+                        stopAtBeginningOfMainSubprogram = false,
+                    }
+
+                    configurations.cpp = {
+                        lldbConfig,
+                        cpptoolsConfig,
+                        gdbConfig,
+                        gdbserverConfig,
+                    }
+                    configurations.c = configurations.cpp
+                    configurations.rust = configurations.cpp
                     configurations.cs = {
                         {
                             type = 'coreclr',
@@ -103,7 +126,86 @@ return {
                     vim.keymap.set({ 'n' }, '<s-<F11>>', dap.step_out, opts)
                 end,
             },
-            { 'mfussenegger/nvim-dap-python', event = 'VeryLazy' },
+            { 'mfussenegger/nvim-dap-python', ft = 'python' },
+            {
+                'theHamsta/nvim-dap-virtual-text',
+                dependencies = {
+                    'nvim-treesitter/nvim-treesitter',
+                },
+                config = function()
+                    require('nvim-dap-virtual-text').setup({
+                        -- enable this plugin (the default)
+                        enabled = true,
+                        -- create commands
+                        -- DapVirtualTextEnable,
+                        -- DapVirtualTextDisable,
+                        -- DapVirtualTextToggle,
+                        -- (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+                        enabled_commands = true,
+                        -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+                        highlight_changed_variables = true,
+                        -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+                        highlight_new_as_changed = false,
+                        -- show stop reason when stopped for exceptions
+                        show_stop_reason = true,
+                        -- prefix virtual text with comment string
+                        commented = false,
+                        -- only show virtual text at first definition (if there are multiple)
+                        only_first_definition = true,
+                        -- show virtual text on all all references of the variable (not only definitions)
+                        all_references = false,
+                        -- clear virtual text on "continue" (might cause flickering when stepping)
+                        clear_on_continue = false,
+                        --- A callback that determines how a variable is displayed or whether it should be omitted
+                        --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
+                        --- @param buf number
+                        --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
+                        --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
+                        --- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
+                        --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+                        display_callback = function(
+                            variable,
+                            buf,
+                            stackframe,
+                            node,
+                            options
+                        )
+                            if options.virt_text_pos == 'inline' then
+                                return ' = ' .. variable.value
+                            else
+                                return variable.name .. ' = ' .. variable.value
+                            end
+                        end,
+                        -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text.
+                        -- Use 'eol' to set to end of line
+                        virt_text_pos = vim.fn.has('nvim-0.10') == 1
+                                and 'inline'
+                            or 'eol',
+
+                        -- experimental features:
+                        --
+                        -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+                        all_frames = false,
+                        -- show virtual lines instead of virtual text (will flicker!)
+                        virt_lines = false,
+                        -- position the virtual text at a fixed window column (starting from the first text column) ,
+                        virt_text_win_col = nil,
+                        -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+                    })
+                end,
+            },
+            {
+                'LiadOz/nvim-dap-repl-highlights',
+                dependencies = {
+                    'nvim-treesitter/nvim-treesitter',
+                },
+                config = function()
+                    require('nvim-dap-repl-highlights').setup()
+                    require('nvim-treesitter.configs').setup({
+                        ensure_installed = { 'dap_repl' },
+                    })
+                end,
+            },
         },
         config = function()
             local dapui = require('dapui')
@@ -194,88 +296,6 @@ return {
             vim.keymap.set({ 'n' }, '<F4>', function()
                 dapui.toggle()
             end, { silent = true })
-        end,
-    },
-    {
-        'theHamsta/nvim-dap-virtual-text',
-        dependencies = {
-            'mfussenegger/nvim-dap',
-            'nvim-treesitter/nvim-treesitter',
-        },
-        event = 'VeryLazy',
-        config = function()
-            require('nvim-dap-virtual-text').setup({
-                -- enable this plugin (the default)
-                enabled = true,
-                -- create commands
-                -- DapVirtualTextEnable,
-                -- DapVirtualTextDisable,
-                -- DapVirtualTextToggle,
-                -- (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-                enabled_commands = true,
-                -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-                highlight_changed_variables = true,
-                -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-                highlight_new_as_changed = false,
-                -- show stop reason when stopped for exceptions
-                show_stop_reason = true,
-                -- prefix virtual text with comment string
-                commented = false,
-                -- only show virtual text at first definition (if there are multiple)
-                only_first_definition = true,
-                -- show virtual text on all all references of the variable (not only definitions)
-                all_references = false,
-                -- clear virtual text on "continue" (might cause flickering when stepping)
-                clear_on_continue = false,
-                --- A callback that determines how a variable is displayed or whether it should be omitted
-                --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
-                --- @param buf number
-                --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
-                --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
-                --- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
-                --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
-                display_callback = function(
-                    variable,
-                    buf,
-                    stackframe,
-                    node,
-                    options
-                )
-                    if options.virt_text_pos == 'inline' then
-                        return ' = ' .. variable.value
-                    else
-                        return variable.name .. ' = ' .. variable.value
-                    end
-                end,
-                -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text.
-                -- Use 'eol' to set to end of line
-                virt_text_pos = vim.fn.has('nvim-0.10') == 1 and 'inline'
-                    or 'eol',
-
-                -- experimental features:
-                --
-                -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-                all_frames = false,
-                -- show virtual lines instead of virtual text (will flicker!)
-                virt_lines = false,
-                -- position the virtual text at a fixed window column (starting from the first text column) ,
-                virt_text_win_col = nil,
-                -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
-            })
-        end,
-    },
-    {
-        'LiadOz/nvim-dap-repl-highlights',
-        dependencies = {
-            'mfussenegger/nvim-dap',
-            'nvim-treesitter/nvim-treesitter',
-        },
-        event = 'VeryLazy',
-        config = function()
-            require('nvim-dap-repl-highlights').setup()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = { 'dap_repl' },
-            })
         end,
     },
 }
