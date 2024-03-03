9cf52a00d6573e5a8dfeabb30d389b2b4e7ba3b5
Peik Etzell <peik.etzell@gmail.com>
2024-03-03 11:56:28 +0200

    Disable quickfix in typst

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -1,4 +1,10 @@
 return {
     'kaarmu/typst.vim',
     ft = 'typst',
+    config = function()
+        vim.g.typst_auto_open_quickfix = 0
+        -- vim.g.typst_embedded_languages = { 'python', 'bash' }
+        -- vim.g.typst_conceal = 1
+        -- vim.g.typst_conceal_math = 1
+    end,
 }
