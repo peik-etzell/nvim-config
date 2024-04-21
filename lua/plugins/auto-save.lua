d3cc2b001014bd31e47f02e99e3b5eb96e60aa09
Peik Etzell <peik.etzell@gmail.com>
2024-04-21 17:00:56 +0300

    Disable auto-save message

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,4 +1,11 @@
 return {
     event = 'VeryLazy',
     'Pocco81/auto-save.nvim',
+    opts = {
+        execution_message = {
+            message = function()
+                return ''
+            end,
+        },
+    },
 }
