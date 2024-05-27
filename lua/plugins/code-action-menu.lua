a0d013c4c666d75028db409c383915e2f2c0415e
Peik Etzell <peik.etzell@gmail.com>
2024-05-27 09:34:06 +0300

    Change code-action-menu to actions-preview

--- lua/plugins/code-action-menu.lua
+++ lua/plugins/code-action-menu.lua
@@ -1,7 +1,20 @@
 return {
     {
+        'aznhe21/actions-preview.nvim',
+        lazy = false,
+        config = function()
+            vim.keymap.set(
+                { 'v', 'n' },
+                '<C-.>',
+                require('actions-preview').code_actions,
+                { silent = true }
+            )
+        end,
+    },
+    {
         'weilbith/nvim-code-action-menu',
         lazy = false,
+        enabled = false,
         config = function()
             vim.g.code_action_menu_window_border = vim.g.border
             vim.keymap.set(
