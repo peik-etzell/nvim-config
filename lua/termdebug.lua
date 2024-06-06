6d357c3ddad5c6af94eeb6b5280ecef66051d10b
Peik Etzell <peik.etzell@gmail.com>
2024-06-06 10:27:25 +0300

    Add termdebug keymaps

--- lua/termdebug.lua
+++ lua/termdebug.lua
@@ -9,3 +9,8 @@ vim.api.nvim_create_autocmd('BufEnter', {
         vim.cmd.packadd('termdebug')
     end,
 })
+
+vim.keymap.set('n', '<F5>', vim.cmd.Continue, { silent = true })
+vim.keymap.set('n', '<F9>', vim.cmd.Break, { silent = true })
+vim.keymap.set('n', '<F10>', vim.cmd.Over, { silent = true })
+vim.keymap.set('n', '<F11>', vim.cmd.Step, { silent = true })
