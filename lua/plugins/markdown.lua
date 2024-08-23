c1adc8561a02e383f22a86be66d52a6184913803
Peik Etzell <peik.etzell@pemamek.com>
2024-08-23 09:22:56 +0300

    Fix markdown-preview build?

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -8,6 +8,7 @@ return {
         },
         ft = { 'markdown' },
         build = function()
+        vim.cmd [[Lazy load markdown-preview.nvim]]
             vim.fn['mkdp#util#install']()
         end,
     },
