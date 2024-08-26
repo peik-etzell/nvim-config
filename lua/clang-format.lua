b3cd1e52228636512facde3ef7550b37b6eaed98
Peik Etzell <peik.etzell@pemamek.com>
2024-08-26 14:24:32 +0300

    Improve clang-format.lua

--- lua/clang-format.lua
+++ lua/clang-format.lua
@@ -1,17 +1,25 @@
-local pattern = { '*.c', '*.cpp', '*.h', '*.hpp', '.cc' }
+local pattern = { '*.c', '*.cpp', '*.h', '*.hpp', '.cc', '.proto' }
 
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     pattern = pattern,
     callback = function(_)
-        local handle = io.popen(
+        local indent_handle = io.popen(
             "clang-format -dump-config | grep ^IndentWidth: | awk '{print $2}'"
         )
-        if handle then
-            local indent_width = tonumber(handle:read('*a'))
-            handle:close()
-
+        if indent_handle then
+            local indent_width = tonumber(indent_handle:read('*a'))
+            indent_handle:close()
             vim.opt_local.tabstop = indent_width
             vim.opt_local.shiftwidth = indent_width
+        end
+
+        local column_handle = io.popen(
+            "clang-format -dump-config | grep ^ColumnLimit: | awk '{print $2}'"
+        )
+        if column_handle then
+            local columns = tonumber(column_handle:read('*a'))
+            column_handle:close()
+            vim.opt_local.colorcolumn = string.format('%d', columns)
         end
     end,
 })
