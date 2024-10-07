bf29c69b83c8d88e90fda1968013bfe6df7d8016
Peik Etzell <peik.etzell@gmail.com>
2024-10-07 12:06:02 +0300

    Re-enable treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -37,12 +37,12 @@ return {
     },
     {
         'nvim-treesitter/nvim-treesitter-context',
-        enabled = false,
+        enabled = true,
         event = 'VeryLazy',
         config = function()
             require('treesitter-context').setup({
                 enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                max_lines = 2, -- How many lines the window should span. Values <= 0 mean no limit.
                 trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
                 min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
                 patterns = {
