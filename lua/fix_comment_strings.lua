f7f170dec28fc14984e57f7c40b660ecc40f0753
Peik Etzell <peik.etzell@gmail.com>
2024-11-24 16:33:19 +0200

    Comment strings with filetype autocmd as well

--- lua/fix_comment_strings.lua
+++ lua/fix_comment_strings.lua
@@ -1,7 +1,7 @@
 -- Change commentstring from default "/* %s */" to "// %s"
 -- For C++, C, header files and C# files
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufFilePost' }, {
-    pattern = { '*.c', '*.cpp', '*.h', '*.hpp', '*.cs', '*.scad' },
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+    pattern = { 'c', 'cpp', 'cuda', 'csharp', 'openscad' },
     callback = function()
         vim.bo.commentstring = '// %s'
     end,
