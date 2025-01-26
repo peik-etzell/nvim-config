500921f05785bd5e7f0c41ec1ce0c352368f45a1
Peik Etzell <peik.etzell@gmail.com>
2025-01-26 15:35:57 +0200

    Always stop and start typst watch on leader-z, do the same for zathura

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -4,6 +4,9 @@ local function typst_stop()
         vim.g.typst_watching = false
         vim.g.typst_watching_job_id = 0
     end
+    if vim.g.zathura_id then
+        vim.fn.jobstop(vim.g.zathura_id)
+    end
 end
 
 local function typst_watch(file)
@@ -27,10 +30,12 @@ end
 
 vim.keymap.set('n', '<leader>z', function()
     local current_file_base = vim.fn.expand('%:p:r')
-    if not vim.g.typst_watching then
-        typst_watch(string.format('%s.typ', current_file_base))
+    typst_watch(string.format('%s.typ', current_file_base))
+    local zathura_id =
+        vim.fn.jobstart({ 'zathura', (current_file_base .. '.pdf') })
+    if zathura_id > 0 then
+        vim.g.zathura_id = zathura_id
     end
-    vim.fn.system(string.format('zathura --fork %s.pdf &', current_file_base))
 end, {})
 
 vim.api.nvim_create_user_command('TypstWatch', function(opts)
