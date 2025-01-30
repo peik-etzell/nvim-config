5328ce3991fce7346f1e79f4815ce044369acd6b
Peik Etzell <peik.etzell@gmail.com>
2025-01-30 09:07:37 +0200

    Revert "Call clang-format asynchronously"
    This reverts commit c2026573c9345714486f4bf79553ae924c95cb2c.

--- lua/clang-format.lua
+++ lua/clang-format.lua
@@ -1,36 +1,23 @@
-local function call_clang_format()
-    local indent = vim.system({
-        'clang-format',
-        '-dump-config',
-        '|',
-        'grep',
-        '^IndentWidth:',
-        '|',
-        'awk',
-        '{print $2}',
-    }, { detach = true })
-
-    local indent_width = tonumber(indent)
-    vim.opt_local.softtabstop = indent_width
-    vim.opt_local.shiftwidth = indent_width
-
-    local columns = vim.system({
-        'clang-format',
-        '-dump-config',
-        '|',
-        'grep',
-        '^ColumnLimit:',
-        '|',
-        'awk',
-        '{print $2}',
-    }, { detach = true })
-
-    vim.opt_local.colorcolumn = columns
-end
-
 vim.api.nvim_create_autocmd({ 'FileType' }, {
     pattern = { 'c', 'cpp', 'cuda' },
     callback = function(_)
-        pcall(call_clang_format)
+        local indent_handle = io.popen(
+            "clang-format -dump-config | grep ^IndentWidth: | awk '{print $2}'"
+        )
+        if indent_handle then
+            local indent_width = tonumber(indent_handle:read('*a'))
+            indent_handle:close()
+            vim.opt_local.softtabstop = indent_width
+            vim.opt_local.shiftwidth = indent_width
+        end
+
+        local column_handle = io.popen(
+            "clang-format -dump-config | grep ^ColumnLimit: | awk '{print $2}'"
+        )
+        if column_handle then
+            local columns = tonumber(column_handle:read('*a'))
+            column_handle:close()
+            vim.opt_local.colorcolumn = string.format('%d', columns)
+        end
     end,
 })
