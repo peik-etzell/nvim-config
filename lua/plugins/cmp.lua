34d0d7b704b4ac642876cf04a87affda262e18d5
Peik Etzell <peik.etzell@gmail.com>
2025-04-07 08:44:26 +0300

    Steal typst buffer completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -104,6 +104,45 @@ return {
                 end,
                 providers = {
                     dap = { name = 'dap', module = 'blink.compat.source' },
+                    buffer = {
+                        name = 'Buffer',
+                        transform_items = function(a, items)
+                            if vim.bo[a.bufnr].filetype ~= 'typst' then
+                                return items
+                            end
+
+                            -- keep case of first char
+                            local keyword = a.get_keyword()
+                            local correct, case
+                            if keyword:match('^%l') then
+                                correct = '^%u%l+$'
+                                case = string.lower
+                            elseif keyword:match('^%u') then
+                                correct = '^%l+$'
+                                case = string.upper
+                            else
+                                return items
+                            end
+
+                            -- avoid duplicates from the corrections
+                            local seen = {}
+                            local out = {}
+                            for _, item in ipairs(items) do
+                                local raw = item.insertText
+                                if raw and raw:match(correct) then
+                                    local text = case(raw:sub(1, 1))
+                                        .. raw:sub(2)
+                                    item.insertText = text
+                                    item.label = text
+                                end
+                                if not seen[item.insertText] then
+                                    seen[item.insertText] = true
+                                    table.insert(out, item)
+                                end
+                            end
+                            return out
+                        end,
+                    },
                 },
             },
 
