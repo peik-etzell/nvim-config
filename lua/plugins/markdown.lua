b9122de62de4c3205709a4b3a8799a4828c402e8
Etzell Peik <peik.etzell@pemamek.com>
2025-08-25 07:32:51 +0300

    Add render-markdown plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -12,4 +12,16 @@ return {
             vim.g.mkdp_filetypes = { 'markdown' }
         end,
     },
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons',
+        },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {
+            completions = { blink = { enabled = true } },
+        },
+    },
 }
