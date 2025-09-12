066989b10ed53a08a2b9f036088dab3257155257
Peik Etzell <peik.etzell@gmail.com>
2025-09-12 12:50:46 +0300

    Close floating windows on <ESC>

--- init.lua
+++ init.lua
@@ -95,6 +95,14 @@ set_keymap('<leader>s', function()
     })
 end)
 
+set_keymap('<ESC>', function()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_config(win).relative == 'win' then
+            vim.api.nvim_win_close(win, false)
+        end
+    end
+end)
+
 set.updatetime = 300
 set.clipboard = 'unnamedplus'
 
