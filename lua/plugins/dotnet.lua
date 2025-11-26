5ff3a8009f7f28fecdd9a4b1cc145facd770fe1b
Peik Etzell <peik.etzell@gmail.com>
2025-11-26 12:26:29 +0200

    Setup netcoredbg path for easy-dotnet

--- lua/plugins/dotnet.lua
+++ lua/plugins/dotnet.lua
@@ -1,7 +1,19 @@
+local mason_pkgs = vim.fn.stdpath('data') .. '/mason/packages'
+local netcoredbg = vim.g.is_windows
+        and mason_pkgs .. '/netcoredbg/netcoredbg/netcoredbg.exe'
+    or vim.g.is_nixos and 'netcoredbg'
+    or mason_pkgs .. '/netcoredbg/netcoredbg'
+
 return {
     'GustavEikaas/easy-dotnet.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
-    config = function()
-        require('easy-dotnet').setup()
-    end,
+    enabled = vim.fn.executable('dotnet') == 1,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+    },
+    opts = {
+        debugger = {
+            bin_path = netcoredbg,
+        },
+    },
 }
