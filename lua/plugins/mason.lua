c5f4f17ed14a2618d57db6d98e57fb921e5358b7
Etzell Peik <peik.etzell@pemamek.com>
2026-04-27 10:40:33 +0300

    Add ensure_installed list and MasonInstallAll cmd

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,6 +5,46 @@ return {
         enabled = vim.g.use_mason,
         event = 'VeryLazy',
         'williamboman/mason.nvim',
-        opts = {},
+        config = function()
+            local options = {
+                max_concurrent_installers = 10,
+                ensure_installed = {
+                    'asm-lsp',
+                    'autotools-language-server',
+                    'bash-language-server',
+                    'black',
+                    'buf',
+                    'clang-format',
+                    'clangd',
+                    'css-lsp',
+                    'deno',
+                    'docker-compose-language-service',
+                    'docker-language-server',
+                    'eslint-lsp',
+                    'html-lsp',
+                    'json-lsp',
+                    'lemminx',
+                    'lua-language-server',
+                    'marksman',
+                    'neocmakelsp',
+                    'shfmt',
+                    'slang',
+                    'stylua',
+                    'superhtml',
+                    'yaml-language-server',
+                },
+            }
+
+            require('mason').setup(options)
+
+            vim.api.nvim_create_user_command('MasonInstallAll', function()
+                for _, pkg_name in ipairs(options.ensure_installed) do
+                    local pkg = require('mason-registry').get_package(pkg_name)
+                    if not pkg:is_installed() then
+                        vim.cmd('MasonInstall ' .. pkg_name)
+                    end
+                end
+            end, {})
+        end,
     },
 }
