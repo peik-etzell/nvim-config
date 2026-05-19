18c69d74823cfbf94861532af4b60c3c26ace27b
Peik Etzell <peik.etzell@gmail.com>
2026-05-19 07:52:23 +0300

    Only use mason if not on nixos

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,6 @@
 require('plugins.picker')
 require('plugins.format')
 require('plugins.completion')
-require('plugins.mason')
 require('plugins.lsp')
 require('plugins.autopairs')
 require('plugins.treesitter')
@@ -9,3 +8,7 @@ require('plugins.comment-toggle')
 require('plugins.schemastore')
 require('plugins.sidebar')
 require('plugins.auto-save')
+
+if not vim.g.is_nixos then
+    require('plugins.mason')
+end
