312e6cececa536fb107edee9027d1c82ca403c7d
Peik Etzell <peik.etzell@pemamek.com>
2026-05-20 08:54:45 +0300

    Disable status in NvimTree

--- lua/plugins/status.lua
+++ lua/plugins/status.lua
@@ -1,6 +1,13 @@
-vim.pack.add {
+vim.pack.add({
     'https://github.com/nvim-mini/mini.statusline',
-}
+})
 
 vim.o.showmode = false
 require('mini.statusline').setup()
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+    pattern = { 'NvimTree' },
+    callback = function()
+        vim.b.ministatusline_disable = true
+    end,
+})
