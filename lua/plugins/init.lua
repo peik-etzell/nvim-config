048d7854c8973d8ffdbf99c0b7c1fab0302893b7
Peik Etzell <peik.etzell@gmail.com>
2026-05-20 08:42:58 +0300

    Add mini.statusline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,6 +9,7 @@ require('plugins.auto-save')
 require('plugins.git')
 require('plugins.dotnet')
 require('plugins.colors')
+require('plugins.status')
 
 if not vim.g.is_nixos then
     require('plugins.mason')
---
+++ lua/plugins/status.lua
@@ -0,0 +1,6 @@
+vim.pack.add {
+    'https://github.com/nvim-mini/mini.statusline',
+}
+
+vim.o.showmode = false
+require('mini.statusline').setup()
