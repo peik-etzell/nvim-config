c34a24614605fc26ba5b4c0e4fa6a566dcbd3300
Peik Etzell <peik.etzell@gmail.com>
2026-05-20 08:03:45 +0300

    Actually require jsonls and yamlls, move schemastore packadd

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,13 +1,18 @@
+vim.pack.add({ 'https://github.com/b0o/schemastore.nvim' })
+
 require('plugins.lsp.clangd')
+require('plugins.lsp.jsonls')
+require('plugins.lsp.yamlls')
 
 local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 vim.pack.add({
     {
         src = 'https://github.com/neovim/nvim-lspconfig',
-    }})
+    },
+})
 
 vim.lsp.config('*', {
     capabilities = capabilities,
-    root_markers = {'.git'},
+    root_markers = { '.git' },
 })
