fcc8c8ef8617417eb78059568c8bf373ffa01768
Peik Etzell <peik.etzell@gmail.com>
2026-05-25 11:34:07 +0300

    Enable treesitter folding and indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -53,5 +53,12 @@ vim.api.nvim_create_autocmd('FileType', {
     pattern = filetypes,
     callback = function()
         vim.treesitter.start()
+
+        -- Folding
+        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo[0][0].foldmethod = 'expr'
+
+        -- Indent
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
     end,
 })
