fb936504cbf95187d6a02a2d74428d0c46e0f14c
Etzell Peik <peik.etzell@pemamek.com>
2026-06-05 13:13:21 +0300

    Always setup easy-dotnet

--- lua/plugins/dotnet.lua
+++ lua/plugins/dotnet.lua
@@ -3,35 +3,27 @@ vim.pack.add({
     'https://github.com/nvim-lua/plenary.nvim',
 })
 
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-    pattern = { 'cs', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets' },
-    callback = function()
-        if vim.fn.executable('dotnet') == 1 then
-            local mason_pkgs = vim.fn.stdpath('data') .. '/mason/packages'
-            local netcoredbg = vim.g.is_windows
-                    and mason_pkgs .. '/netcoredbg/netcoredbg/netcoredbg.exe'
-                or vim.g.is_nixos and 'netcoredbg'
-                or mason_pkgs .. '/netcoredbg/netcoredbg'
+if vim.fn.executable('dotnet') == 1 then
+    local mason_pkgs = vim.fn.stdpath('data') .. '/mason/packages'
+    local netcoredbg = vim.g.is_windows
+            and mason_pkgs .. '/netcoredbg/netcoredbg/netcoredbg.exe'
+        or vim.g.is_nixos and 'netcoredbg'
+        or mason_pkgs .. '/netcoredbg/netcoredbg'
 
-            if vim.fn.executable(netcoredbg) == 0 then
-                print('netcoredbg is not executable')
-            end
+    if vim.fn.executable(netcoredbg) == 0 then
+        print('netcoredbg is not executable')
+    end
 
-            require('easy-dotnet').setup({
-                lsp = {
-                    auto_refresh_codelens = false,
-                },
-                debugger = {
-                    bin_path = netcoredbg,
-                },
-            })
+    local opts = {
+        lsp = {
+            auto_refresh_codelens = false,
+        },
+        debugger = {
+            bin_path = netcoredbg,
+        },
+        notifications = { handler = false },
+    }
+    require('easy-dotnet').setup(opts)
 
-            vim.keymap.set(
-                'n',
-                '<leader>dn',
-                ':Dotnet<CR>',
-                { desc = '[D]ot[N]et' }
-            )
-        end
-    end,
-})
+    vim.keymap.set('n', '<leader>dn', ':Dotnet<CR>', { desc = '[D]ot[N]et' })
+end
