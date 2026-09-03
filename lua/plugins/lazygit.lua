cd683e2ef2281a5fa7c0cac337d0beafe42a55ba
Etzell Peik <peik.etzell@pemamek.com>
2026-09-03 08:43:28 +0300

    Re-add lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,15 +1,13 @@
-return {
-    {
-        'kdheepak/lazygit.nvim',
-        -- optional for floating window border decoration
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-        },
-        config = function()
-            vim.g.lazygit_floating_window_scaling_factor = 1.0
-        end,
-        keys = {
-            { '<leader>lz', ':LazyGit<CR>', desc = 'Open LazyGit UI' },
-        },
-    },
-}
+vim.pack.add({
+    'https://github.com/kdheepak/lazygit.nvim',
+    'https://github.com/nvim-lua/plenary.nvim',
+})
+
+vim.g.lazygit_floating_window_scaling_factor = 1.0
+
+vim.keymap.set(
+    'n',
+    '<leader>lz',
+    require('lazygit').lazygit,
+    { desc = 'Open LazyGit UI' }
+)
